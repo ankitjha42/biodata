21bb9fa418a88aec6ff6dfa9e9ae48d0eb5f923c
ankitjha42 <ankit_jha@srmap.edu.in>
2024-03-01 02:23:12 +0530

    biodata

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed3 = new Typed('.front', {
+  strings: [' <i>Frontend Developer!</i>', 'B.Tech Student', 'Nice person'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  smartBackspace: true, // this is a default
+  loop: true,
+  
+});
+// Get the element containing the moving text
+var textElement = document.getElementById("front");
